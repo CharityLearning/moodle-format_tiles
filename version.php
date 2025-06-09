089bdc7d01c3ffb41ae6c9a3f7ee8d3ad30e54d5
David Watson <david@evolutioncode.uk>
2025-06-09 13:56:13 +0100

    Version 5.0.0.17 (2025060267)

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025060266;     // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version = 2025060267;     // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires = 2025040800;    // Moodle 5.0.
 $plugin->component = 'format_tiles';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "5.0.0.16";
+$plugin->release = "5.0.0.17";
 $plugin->supported = [500, 500]; // Supported Moodle version range.
